9e6078efa3cb0add1dc29d3f979030ed532a221f
Pedro Lucas <pedro.lucx@gmail.com>
2022-11-24 08:26:01 -0300

    feature (implementin code highlight)

--- src/pages/ScriptArticle/index.js
+++ src/pages/ScriptArticle/index.js
@@ -1,23 +1,31 @@
 import React, { useEffect } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
+import { getScriptData } from '../../store/scriptsContent';
+
 import { AboutContent, TitleScript } from '../../common/components/Contents';
 import { getQueryParams } from '../../common/utils/routerHelpers';
-import { getScriptData } from '../../store/scriptsContent';
+
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import CodeBlock from './components/CodeHighlight';
 
 function ScriptArticle(){
     const dispatch = useDispatch();
     const content = useSelector((state) => state.scriptsContent.current_script);
     const scriptTitle = content.name?.split("_").join(" ");
 
-    const lastScriptCommit = content.script?.last_commit.commit.author.date;
     const decodedTextContent = content.script ? atob(content.script.data_text.content).split("\n").join("<br>") : '';
-    const decodedCodeContent = content.script ? atob(content.script.data_code.content).split("\n").join("<br>") : '';
+    const decodedCodeContent = content.script ? atob(content.script.data_code.content) : '';
+
     
     useEffect(() => {
         const { project_name, script_name }= getQueryParams();
         const [ projectName, scriptName] = [ project_name, script_name ];
 
         dispatch(getScriptData({projectName, scriptName}));
+
+        Prism.highlightAll();
       }, []);
 
     return (
@@ -29,9 +37,10 @@ function ScriptArticle(){
                 dangerouslySetInnerHTML={{__html: decodedTextContent}}
             />
 
-            <div className="p-2 p-lg-3" style={{backgroundColor: '#00000014',borderRadius: '15px'}}>
-                <code dangerouslySetInnerHTML={{__html: decodedCodeContent}} />
-            </div>
+            <CodeBlock
+                code={decodedCodeContent}
+                language={content?.script?.script_language}>
+            </CodeBlock>
         </div>
         
     )
